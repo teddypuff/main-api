a760c1835771aa5fd7bbeeb3d99bb87726a3d725
UCrypt Developer <teddypuff2025@gmail.com>
2025-02-17 15:54:56 +0000

    block websocket message senders

--- src/notification/gateways/notification.gateway.ts
+++ src/notification/gateways/notification.gateway.ts
@@ -23,10 +23,23 @@ export class NotificationGateway {
   @WebSocketServer()
   server: Server;
 
+  afterInit(server: Server) {
+    this.logger.log('WebSocket server initialized');
+
+    // block user messages
+    server.use((socket, next) => {
+      socket.onAny((event, ...args) => {
+        this.logger.warn(`Blocked event: ${event} from client ${socket.id}`);
+        socket.disconnect(); // disconnect the user
+      });
+      next();
+    });
+  }
+
   sendNotification(event: string, message: any) {
-    this.logger.log(
-      `Broadcasting event: ${event}, message: ${JSON.stringify(message)}`,
-    );
+    // this.logger.log(
+    //   `Broadcasting event: ${event}, message: ${JSON.stringify(message)}`,
+    // );
     this.server.emit(event, message);
   }
 }
